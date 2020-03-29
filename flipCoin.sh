079993beece99baad66137ceaae939513e6fd605
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-29 17:26:52 +0530

    ADDED: winningCombinations functions

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,10 +1,13 @@
 #!/bin/bash -x
 #Constants
 declare -A singletCount
+declare -a singletSortedCount
 declare -A singletPercentage
 declare -A doubletCount
+declare -a doubletSortedCount
 declare -A doubletPercentage
 declare -A tripletCount
+declare -a tripletSortedCount
 declare -A tripletPercentage
 function flipCoin() {
 	if [ $(($RANDOM%2)) -eq 1 ]
@@ -62,8 +65,47 @@ function tripletCombinations() {
 	done
 	getPercentage tripletCount tripletPercentage $1
 }
+
+function getSortedValues() {
+	declare -n combinationsCount=$1
+	count=0
+	declare -n newArray=$2 
+	for key in "${!combinationsCount[@]}"
+	do
+
+		newArray[count]=${combinationsCount[$key]}
+		((count++))
+	done
+	echo ${newArray[@]}
+	for ((i=0;i<count;i++))
+	do
+		echo ${newArray[i]}
+	done | sort -r
+}
+function getWinningCombinations() {
+	declare -n combinationsDict=$1
+	declare -n sortedArray=$2
+	echo ${sortedArray[@]}
+	max=${sortedArray}
+	winner=""
+	for key in "${!combinationsDict[@]}"
+	do
+		value=${combinationsDict[$key]}
+		if [ $value -eq $max ]
+		then
+			winner=$key" "$winner
+		fi
+	done
+	echo $winner
+}
 echo "Welcome to flip coin simulation"
 flipCoin
 singletCombinations 10
+getSortedValues singletCount singletSortedCount
+getWinningCombinations singletCount singletSortedCount
 doubletCombinations 10
-tripletCombinations 10
+getSortedValues doubletCount doubletSortedCount
+getWinningCombinations doubletCount doubletSortedCount
+#tripletCombinations 10
+#getWinningCombinations doubletCount doubletSortedCount
+#getWinningCombinations tripletCount tripletSortedCount
