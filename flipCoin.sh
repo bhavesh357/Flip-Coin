3a6ce41bc024a72c54783a612c0fd0af83fea903
Bhavesh Kadam <bkadam357@gmail.com>
2020-03-29 13:29:04 +0530

    ADDED: singletCombinations function

--- flipCoin.sh
+++ flipCoin.sh
@@ -1,4 +1,7 @@
 #!/bin/bash -x
+#Constants
+declare -A singletCount
+declare -A singletPercentage
 function flipCoin() {
 	if [ $(($RANDOM%2)) -eq 1 ]
 	then 
@@ -7,6 +10,17 @@ function flipCoin() {
 		echo "T"
 	fi
 }
-
+function singletCombinations() {
+	singletCount["H"]=0
+	singletCount["T"]=0
+	for((i=0;i<$1;i++))
+	do
+		result=$(flipCoin)
+		singletCount[$result]=$((${singletCount[$result]}+1))
+	done
+	echo ${!singletCount[@]}
+	echo ${singletCount[@]}
+}
 echo "Welcome to flip coin simulation"
 flipCoin
+singletCombinations 10
